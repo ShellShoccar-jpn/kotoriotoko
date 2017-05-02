dd4ae18a36681c536a33f2b68e7c03ceaa37e228
321516 <321516.shellshoccar@gmail.com>
2017-05-03 01:41:17 +0900

    BIN/twmediup.sh: Fixed a bug about incompatiblity with POSIX

--- BIN/twmediup.sh
+++ BIN/twmediup.sh
@@ -4,7 +4,7 @@
 #
 # TWMEDIUP.SH : Upload An Image or Video File To Twitter
 #
-# Written by Shell-Shoccar Japan (@shellshoccarjpn) on 202017-05-02
+# Written by Shell-Shoccar Japan (@shellshoccarjpn) on 2017-05-03
 #
 # This is a public-domain software (CC0). It means that all of the
 # people can use this for any purposes with no restrictions at all.
@@ -29,7 +29,7 @@ export UNIX_STD=2003  # to make HP-UX conform to POSIX
 print_usage_and_exit () {
   cat <<-USAGE 1>&2
 	Usage   : ${0##*/} <file>
-	Version : 202017-05-02 21:11:01 JST
+	Version : 2017-05-03 01:24:55 JST
 	USAGE
   exit 1
 }
@@ -124,7 +124,7 @@ for arg in "$@"; do
     'mpg4') exec "$Homedir/BIN/twvideoup.sh" "$arg";; # (will not come back)
     *)      error_exit 1 "Unsupported file format: $arg";;
   esac
-  s=$(printf '%s' "$arg" | sed 's/\\/\\\\/g' | sed 's/"/\\"/'g)
+  s=$(printf '%s\n' "$arg" | sed 's/\\/\\\\/g' | sed 's/"/\\"/'g)
   mimemake_args="$mimemake_args -Ft media \"$type\" \"$s\""
 done
 
