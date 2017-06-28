443703f8cfa039f3f723d46b47a2a7b1e77fa1d0
richmikan <richmikan@richlab.org>
2017-06-28 23:22:08 +0900

    UTL/SRC/sleep.c: Replaced newer one

--- UTL/SRC/sleep.c
+++ UTL/SRC/sleep.c
@@ -1,52 +1,90 @@
-/*  sleep command which accepts a decimal time
- *
- *  2016/03/08 written by ShellShoccar Japan
- *
-*  The software is PUBLIC DOMAIN (CC0).
- */
+/*####################################################################
+#
+# SLEEP - Sleep Command Which Supported Non-Integer Numbers
+#
+# USAGE   : sleep <seconds>
+# Args    : second ... The number of second to sleep for. You can
+#                      give not only an integer number but also a
+#                      non-integer number here.
+# Retuen  : Return 0 only when succeeded to sleep
+#
+# Written by Shell-Shoccar Japan (@shellshoccarjpn) on 2017-05-11
+#
+# This is a public-domain software (CC0). It means that all of the
+# people can use this for any purposes with no restrictions at all.
+# By the way, We are fed up with the side effects which are brought
+# about by the major licenses.
+#
+####################################################################*/
+
+
+/*####################################################################
+# Initial Configuration
+####################################################################*/
+
+/*=== Initial Setting ==============================================*/
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define WRN(fmt,args...) fprintf(stderr,fmt,##args)
 
-void usage (char* pszMypath);
-void errmsg(char* pszMypath);
+char* pszMypath;
+
+/*=== Define the functions for printing usage and error ============*/
+void print_usage_and_exit(void) {
+  int  i;
+  int  iPos = 0;
+  for (i=0; *(pszMypath+i)!='\0'; i++) {
+    if (*(pszMypath+i)=='/') {iPos=i+1;}
+  }
+  WRN("USAGE   : %s <seconds>\n",pszMypath+iPos                               );
+  WRN("Args    : second ... The number of second to sleep for. You can\n"     );
+  WRN("                     give not only an integer number but also a\n"     );
+  WRN("                     non-integer number here.\n"                       );
+  WRN("Retuen  : Return 0 only when succeeded to sleep\n"                     );
+  WRN("Version : 2017-05-11 06:22:12 JST\n"                                   );
+  WRN("          (POSIX C language)\n"                                        );
+  exit(1);
+}
+void error_exit(int iErrno, char* szMessage) {
+  int  i;
+  int  iPos = 0;
+  for (i=0; *(pszMypath+i)!='\0'; i++) {
+    if (*(pszMypath+i)=='/') {iPos=i+1;}
+  }
+  WRN("%s: %s\n",pszMypath+iPos, szMessage);
+  exit(iErrno);
+}
+
+
+/*####################################################################
+# Main
+####################################################################*/
 
 int main(int argc, char *argv[]) {
+
+  /*=== Initial Setting ============================================*/
   struct timespec tspcSleeping_time;
   double dNum;
   char   szBuf[2];
   int    iRet;
 
-  if (argc != 2                                   ) {usage(argv[0]);}
-  if (sscanf(argv[1], "%lf%1s", &dNum, szBuf) != 1) {usage(argv[0]);}
-  if (dNum > INT_MAX                              ) {usage(argv[0]);}
-  if (dNum <= 0                                   ) {return(0);     }
+  pszMypath = argv[0];
 
+  /*=== Parse options ==============================================*/
+  if (argc != 2                                   ) {print_usage_and_exit();}
+  if (sscanf(argv[1], "%lf%1s", &dNum, szBuf) != 1) {print_usage_and_exit();}
+  if (dNum > INT_MAX                              ) {print_usage_and_exit();}
+
+  /*=== Sleep ======================================================*/
+  if (dNum <= 0                                   ) {exit(0);               }
   tspcSleeping_time.tv_sec  = (time_t)dNum;
   tspcSleeping_time.tv_nsec = (dNum - tspcSleeping_time.tv_sec) * 1000000000;
 
   iRet = nanosleep(&tspcSleeping_time, NULL);
-  if (iRet != 0) {errmsg(argv[0]);}
-  return(iRet);
-}
+  if (iRet != 0) {error_exit(iRet,"Error happend while nanosleeping\n");}
 
-void usage(char* pszMypath) {
-  int  i;
-  int  iPos = 0;
-  for (i=0; *(pszMypath+i)!='\0'; i++) {
-    if (*(pszMypath+i)=='/') {iPos=i+1;}
-  }
-  fprintf(stderr, "Usage : %s <seconds>\n",pszMypath+iPos);
-  exit(1);
-}
-
-void errmsg(char* pszMypath) {
-  int  i;
-  int  iPos = 0;
-  for (i=0; *(pszMypath+i)!='\0'; i++) {
-    if (*(pszMypath+i)=='/') {iPos=i+1;}
-  }
-  fprintf(stderr, "%s: Error happend while nanosleeping\n",pszMypath+iPos);
-  exit(1);
+  /*=== Finish =====================================================*/
+  exit(0);
 }
